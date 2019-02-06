3135f759a011ffa3587549998c54e9bbce7f762f
Adam Kafka <akafka@tesla.com>
2019-02-05 17:44:09 -0800

    Context is a functor

--- src/main/scala/stream/Play.scala
+++ src/main/scala/stream/Play.scala
@@ -5,18 +5,33 @@ import akka.stream.OverflowStrategy
 import akka.stream.scaladsl.Source
 import cats._
 import cats.data._
+import cats.implicits._
 import stream.Play.{CommittableElement, CommittableOffset, Context, Incoming}
 
-import scala.util.Try
+import scala.concurrent.Future
 import scala.concurrent.duration._
+import scala.language.higherKinds
+import scala.util.Try
 
 object Play {
-  final case class Context[C, A](context: C, get: A)
+  final case class Context[C, A](context: C, get: A) {
+    def map[B](f: A => B): Context[C, B] = Context(context, f(get))
+    def traverse[G[_]: Applicative, B](f: A => G[B]): G[Context[C, B]] = Applicative[G].map(f(get))(Context(context, _))
+  }
 
   object Context {
     implicit def functor[C]: Functor[Context[C, ?]] = {
       new Functor[Context[C, ?]] {
         override def map[A, B](fa: Context[C, A])(f: (A) => B): Context[C, B] = Context(fa.context, f(fa.get))
+      }
+    }
+
+    implicit def traverse[C]: Traverse[Context[C, ?]] = {
+      new Traverse[Context[C, ?]] {
+        override def traverse[G[_]: Applicative, A, B](fa: Context[C, A])(f: A => G[B]): G[Context[C, B]] = fa.traverse(f)
+        override def foldLeft[A, B](fa: Context[C, A], b: B)(f: (B, A) => B): B = f(b, fa.get)
+        // TODO: Make this function actually lazy
+        override def foldRight[A, B](fa: Context[C, A], lb: Eval[B])(f: (A, Eval[B]) => Eval[B]): Eval[B] = f(fa.get, lb)
       }
     }
   }
@@ -26,6 +41,7 @@ object Play {
   type CommittableElement[A] = Context[CommittableOffset, A]
 
   val c: Functor[CommittableElement] = Functor[CommittableElement]
+  val t: Traverse[CommittableElement] = Traverse[CommittableElement]
 
   case class Incoming(hexString: String)
   case class Parsed(num: Int)
@@ -34,6 +50,7 @@ object Play {
 
   sealed trait Errors
   case class ParseError(input: Incoming) extends Errors
+  case class NotInWhitelist() extends Errors
 
   def parse(in: Incoming): Either[Errors, Parsed] = {
     Try(in.hexString.toInt).fold(
@@ -42,14 +59,20 @@ object Play {
     )
   }
   def cube(p: Parsed): Cubed = Cubed(p.num ^ 3)
+  def whiteListAsync(cubed: Cubed): Future[Either[NotInWhitelist, Cubed]] = {
+    if (cubed.num < 1000) Future.successful(Either.right(cubed))
+    else Future.successful(Either.left(NotInWhitelist()))
+  }
   def isEven(p: Cubed): Result = if (p.num % 2 == 0) Result(true) else Result(false)
 
+  // Note: Would be syntactically cleaner with a StreamT/FlowT class
   val src: Source[CommittableElement[Either[Errors, Result]], NotUsed] = {
     KafkaSource()
       .map(c.map(_)(parse))
       .buffer(1000, OverflowStrategy.backpressure)
       .map(EitherT.apply)
       .map(_.map(cube))
+      .mapAsync(5)(e => e.traverse(whiteListAsync)).map(_.subflatMap(identity))
       .throttle(1, 1.second)
       .map(_.map(isEven))
       .map(_.value)
